1efd283e13a220c786b709f6ec5d6235d729e73e
Dominik Němeček <dnemecek52@gmail.com>
2025-10-11 12:29:38 +0200

    Update hello-world.c

--- 00-main/ulohy-z-hodiny/01-zaklady-programovani-c/hello-world.c
+++ 00-main/ulohy-z-hodiny/01-zaklady-programovani-c/hello-world.c
@@ -1,6 +1,16 @@
+// Tento program vypíše text "Hello world!" do konzole.
+
+// Direktiva preprocesoru – připojuje standardní knihovnu pro vstup a výstup.
+// Díky ní můžeme používat funkce jako printf().
 #include <stdio.h>
 
+// Hlavní funkce programu – tady vždy začíná vykonávání programu.
 int main() {
-    printf("Hello world!\\n");
+
+    // Funkce printf() slouží k výpisu textu do konzole.
+    // \n znamená "nový řádek" – kurzor se po výpisu přesune na další řádek.
+    printf("Hello world!\n");
+
+    // return 0 říká, že program skončil úspěšně bez chyby.
     return 0;
 }
