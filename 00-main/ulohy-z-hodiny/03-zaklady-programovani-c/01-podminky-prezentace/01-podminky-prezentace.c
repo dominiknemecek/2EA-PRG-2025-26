827ec6d48645115e3dc1730a2a485dd504c17b1c
Dominik Němeček <dnemecek52@gmail.com>
2025-10-26 10:11:45 +0100

    Refactor code with comments and structure
    Added comments and structured code for clarity.

--- 00-main/ulohy-z-hodiny/03-zaklady-programovani-c/01-podminky-prezentace/01-podminky-prezentace.c
+++ 00-main/ulohy-z-hodiny/03-zaklady-programovani-c/01-podminky-prezentace/01-podminky-prezentace.c
@@ -1,1 +1,17 @@
-#include <stdio.h>\n\nint main() {\n    printf("Hello world!\\n");\n    return 0;\n}
+/*---- | 05 PODMÍNKY A VĚTVENÍ PROGRAMU | ----
+==============================================
+Zdrojový kód: 01_Podminky_Prezentace
+*/
+
+#include <stdio.h> // Zahrnuje standardní knihovnu pro vstup a výstup
+
+int x = 6; // Definujeme globální proměnnou x a přiřazujeme jí hodnotu 6
+
+int main() // Hlavní funkce programu, kde začíná vykonávání kódu
+{
+    if (x > 5) // Podmínka: pokud je hodnota x větší než 5
+    {
+        printf("%d", x); // Pokud je podmínka splněna, vytiskne hodnotu x
+    }
+    return 0; // Ukončení hlavní funkce, návratová hodnota 0 značí úspěšné dokončení programu
+}
