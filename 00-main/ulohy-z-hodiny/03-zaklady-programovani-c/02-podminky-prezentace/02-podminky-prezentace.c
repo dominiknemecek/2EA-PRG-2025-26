a741d593523a6c3f235adeda45b487e989e12a73
Dominik Němeček <dnemecek52@gmail.com>
2025-10-26 10:12:52 +0100

    Update 02-podminky-prezentace.c

--- 00-main/ulohy-z-hodiny/03-zaklady-programovani-c/02-podminky-prezentace/02-podminky-prezentace.c
+++ 00-main/ulohy-z-hodiny/03-zaklady-programovani-c/02-podminky-prezentace/02-podminky-prezentace.c
@@ -1,1 +1,24 @@
-#include <stdio.h>\n\nint main() {\n    printf("Hello world!\\n");\n    return 0;\n}
+/*---- | 05 PODMÍNKY A VĚTVENÍ PROGRAMU | ----
+==============================================
+Zdrojový kód: 02_Podminky_Prezentace
+*/
+
+#include <stdio.h> // Zahrnuje standardní knihovnu pro vstup a výstup
+
+int x; // Definujeme proměnnou x
+
+int main()
+{
+    printf("Zadej cislo: \n"); // Vytiskne na obrazovku výzvu k zadání čísla
+    scanf("%d", &x); // Načte celé číslo z uživatelského vstupu a uloží ho do proměnné x
+    
+    if (x > 5) // Podmínka: pokud je hodnota x větší než 5
+    {
+        printf("x je vetsi"); // Pokud je podmínka splněna, vytiskne "x je vetsi"
+    }
+    else // Pokud není podmínka splněna (x je menší nebo rovno 5)
+    {
+        printf("x je mensi"); // Vytiskne "x je mensi"
+    }
+    return 0; // Ukončení hlavní funkce, návratová hodnota 0 značí úspěšné dokončení programu
+}
