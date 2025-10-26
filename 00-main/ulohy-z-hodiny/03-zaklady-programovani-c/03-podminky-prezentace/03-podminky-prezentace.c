919e037889f6e80a03fe44ca14bcba6f15bf7047
Dominik Němeček <dnemecek52@gmail.com>
2025-10-26 10:13:33 +0100

    Update 03-podminky-prezentace.c

--- 00-main/ulohy-z-hodiny/03-zaklady-programovani-c/03-podminky-prezentace/03-podminky-prezentace.c
+++ 00-main/ulohy-z-hodiny/03-zaklady-programovani-c/03-podminky-prezentace/03-podminky-prezentace.c
@@ -1,1 +1,21 @@
-#include <stdio.h>\n\nint main() {\n    printf("Hello world!\\n");\n    return 0;\n}
+/*---- | 05 PODMÍNKY A VĚTVENÍ PROGRAMU | ----
+==============================================
+Zdrojový kód: 03_Podminky_Prezentace
+*/
+
+#include <stdio.h>  // Zahrnutí standardní knihovny pro vstup a výstup
+
+int main()  // Hlavní funkce programu, která se spouští při jeho startu
+{
+    int x;  // Deklarace proměnné 'x', která bude sloužit k uložení zadaného čísla
+    printf("Zadej cislo: \n");  // Výzva pro uživatele, aby zadal číslo
+    scanf("%d", &x);  // Načtení čísla od uživatele a jeho uložení do proměnné 'x'
+    
+    // Podmínka kontrolující, zda je číslo sudé nebo liché
+    if (x % 2 == 0)  // Pokud je zbytek po dělení čísla 2 nulový, číslo je sudé
+        printf("Sude");  // Pokud je podmínka splněna (číslo je sudé), vypíše se "Sude"
+    else  // Pokud podmínka splněna není (číslo je liché)
+        printf("Liche");  // Vypíše se "Liche"
+
+    return 0;  // Konec programu, návratová hodnota 0 značí úspěšné dokončení
+}
